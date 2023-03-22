dc87794a7c1403ca7c22d2738dad687bac7d5731
Gleb Gafeev <gleb.gafeev@mail.ru>
2023-03-22 19:50:11 +0300

    base folders

--- app/src/main/java/com/example/tinkofflab2023/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tinkofflab2023/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tinkofflab2023
+package com.example.tinkofflab2023.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tinkofflab2023.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
