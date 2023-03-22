e45aef78d0f5b4315b5e51de472ca8884422d32e
Гафеев Глеб <gleb.gafeev@mail.ru>
2023-03-22 18:43:29 +0300

    remote

---
+++ app/src/main/java/com/example/tinkofflab2023/presentation/fragment/MainFragment.kt
@@ -0,0 +1,16 @@
+package com.example.tinkofflab2023.presentation.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.tinkofflab2023.R
+import com.example.tinkofflab2023.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private val binding: FragmentMainBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
